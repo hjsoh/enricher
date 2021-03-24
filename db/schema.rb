deb7756e14c780b0730626eca0086717bd5f2722
Wong Zhi Kai <wzhikai@gmail.com>
2021-03-24 21:53:04 +0800

    fixed the ruby version file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_115557) do
+ActiveRecord::Schema.define(version: 2021_03_24_124137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "classrooms", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_active"
+    t.string "academic_year"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_classrooms_on_user_id"
+  end
+
+  create_table "guardianships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "student_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["student_id"], name: "index_guardianships_on_student_id"
+    t.index ["user_id"], name: "index_guardianships_on_user_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.boolean "is_active"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +55,7 @@ ActiveRecord::Schema.define(version: 2021_03_24_115557) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "classrooms", "users"
+  add_foreign_key "guardianships", "students"
+  add_foreign_key "guardianships", "users"
 end
