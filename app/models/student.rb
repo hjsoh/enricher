ffab97b4de4d2ba83e9de0a1f582ec167ff031bb
Shu-Yi Lim <shu.lim27@gmail.com>
2021-03-26 16:59:30 +0800

    added associations and validations to student and users model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
-  has_many :enrollments, dependent: :destroy
+  has_many :classrooms, through: :enrollments, dependent: :destroy
   has_many :guardianships, dependent: :destroy
 
   validates :name, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :role, presence: true, inclusion: { in: ['teacher', 'parent'] }
+  validates :name, presence: true
+
+  has_many :students, through: :guardianships
+  has_many :classrooms
 end
