4bed4b6035a6349fbf9c057fa0396c75d43e3ab5
Margarita Maltseva <margarita.maltseva@protonmail.com>
2021-03-27 11:26:47 +0800

    Ordinary chats done

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,15 +5,15 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_user
-    
-    if @message.save
-      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
-    else
-        render "chatrooms/show"
+
+  if @message.save
+    redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
+  else
+    render "chatrooms/show"
     end
   end
-    
-    def message_params
-        params.require(:message).permit(:content)
-    end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
