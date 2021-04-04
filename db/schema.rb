197dc308d96f755f867781da83be160231cea4a5
Shu-Yi Lim <shu.lim27@gmail.com>
2021-04-04 14:58:48 +0800

    added announcement model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_022839) do
+ActiveRecord::Schema.define(version: 2021_04_04_065338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "announcements", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "appointments", force: :cascade do |t|
     t.bigint "user_id", null: false
