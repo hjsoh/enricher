01b099c7aef5e5e7d91963e97378503f5a352300
Shu-Yi Lim <shu.lim27@gmail.com>
2021-04-04 15:59:43 +0800

    fixed associations

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,5 +1,6 @@
 class Announcement < ApplicationRecord
-  has_one :classroom_announcement, dependent: :destroy
+  has_many :classroom_announcements
+  has_many :classrooms, through: :classroom_announcements, dependent: :destroy
 
   validates :title, presence: true
 end
