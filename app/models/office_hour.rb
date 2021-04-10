d0f96509df256437532050d99711baeda2ffdc77
hwajie soh <soh_hwajie@hotmail.com>
2021-04-10 17:37:27 +0800

    formatted simple cal

--- app/models/office_hour.rb
+++ app/models/office_hour.rb
@@ -1,4 +1,16 @@
 class OfficeHour < ApplicationRecord
   belongs_to :user
   has_one :appointment
+
+  validates :start_time, :end_time, presence: true
+
+  default_scope -> { order(:start_time) }
+
+  def time
+    "#{start_time.strftime('%I:%M %p')} - #{end_time.strftime('%I:%M %p')}"
+  end
+
+  def multi_days?
+    (end_time.to_date - start_time.to_date).to_i >= 1
+  end
 end
