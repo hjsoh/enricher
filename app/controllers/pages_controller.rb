2c110290562e0f42d5facd098465e97fe8a3926e
Shu-Yi Lim <shu.lim27@gmail.com>
2021-04-14 21:49:00 +0800

    saving

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,19 +3,19 @@ class PagesController < ApplicationController
 
   def home
     if user_signed_in?
-      if current_user.role == 'parent'
+      # if current_user.role == 'parent'
 
         @navbar = true
         @footer = true
 
-        render :parent
-      else
+        # render :parent
+      # else
         @user = current_user
         @announcements = @user.announcements.order(created_at: :desc).first(4)
         @tickets = @user.tickets.order(created_at: :desc).first(4)
         @office_hours = @user.office_hours.order(created_at: :desc).first(4)
         render :teacher
-      end
+      # end
     else
       redirect_to new_user_session_path
     end
