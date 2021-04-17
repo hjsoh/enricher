27a18b31ebae69e827a20e81138aeaa3a34e4fc3
Shu-Yi Lim <shu.lim27@gmail.com>
2021-04-17 16:30:22 +0800

    parent dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,14 +3,23 @@ class PagesController < ApplicationController
 
   def home
     if user_signed_in?
+        # @navbar = true
+        # @footer = true
+        @user = current_user
+
       if current_user.role == 'parent'
-
-        @navbar = true
-        @footer = true
-
+        @classrooms = []
+        @announcements = []
+        @user.students.each do |s|
+          s.classrooms.each do |c|
+            @classrooms << c
+          end
+        end
+        @classrooms.each do |c|
+          @announcements << c.announcements
+        end
         render :parent
       else
-        @user = current_user
         @announcements = @user.announcements.order(created_at: :desc).first(4)
         @tickets = @user.tickets.order(created_at: :desc).first(4)
         @office_hours = @user.office_hours.order(created_at: :desc).first(4)
