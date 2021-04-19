5f12b23118c141b584c9262a9f7d48fca71e146e
Wong Zhi Kai <wzhikai@gmail.com>
2021-04-19 19:07:34 +0800

    FIXED MAIL

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -11,12 +11,12 @@
 # }
 
 
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  domain: 'https://enricher2.herokuapp.com/',
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
-  authentication: :login,
-  enable_starttls_auto: true
-}
+# ActionMailer::Base.smtp_settings = {
+#   address: 'smtp.sendgrid.net',
+#   port: 587,
+#   domain: 'heroku.com',
+#   user_name: ENV['SENDGRID_USERNAME'],
+#   password: ENV['SENDGRID_PASSWORD'],
+#   authentication: :login,
+#   enable_starttls_auto: true
+# }
