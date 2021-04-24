93ec9709c31976fb8828eee5e36700e1ada7e5cd
Wong Zhi Kai <wzhikai@gmail.com>
2021-04-24 17:44:45 +0800

    spaghetti code to get unread announcements in a helper method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,12 +11,25 @@ class PagesController < ApplicationController
 
         @user = current_user
         @announcements = @user.student_announcements.order(created_at: :desc)
+        @unread_announcements = @user.student_announcements.where("created_at >= ?", @user.last_sign_in_at)
+        raise
         @tickets = @user.tickets.where.not(status: 'Completed').order(created_at: :desc)
         @appointments = @user.appointments.joins(:office_hour).where("office_hours.start_time > ?", DateTime.now).order('office_hours.start_time ASC')
         render :parent
       else
         @user = current_user
         @announcements = @user.announcements.order(created_at: :desc)
+        @classrooms = @user.classrooms
+        @classroom_announcements = []
+        @classrooms.each do |classroom|
+          if !classroom.classroom_announcements.empty?
+            @classroom_announcements << classroom.classroom_announcements
+          end
+        end
+        @filtered_announcements = @classroom_announcements.flatten
+
+        @unread_announcements = @filtered_announcements.reject{ |announcement| announcement.created_at <= @user.last_sign_in_at }
+
         @tickets = @user.teacher_tickets.where.not(status: 'Completed').order(created_at: :desc)
         @appointments = @user.teacher_appointments.joins(:office_hour).where("office_hours.start_time > ?", DateTime.now).order('office_hours.start_time ASC')
 
