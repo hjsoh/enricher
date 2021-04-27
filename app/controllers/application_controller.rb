f8b9b6cff7c89f68a690af6580e0cb02f46636d6
hwajie soh <soh_hwajie@hotmail.com>
2021-04-27 16:26:21 +0800

    removed unread methods except for announcements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,21 +26,6 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 
-  def get_announcements(user)
-    @user = user
-    @announcements = @user.announcements.order(created_at: :desc)
-    @classrooms = @user.classrooms
-    @classroom_announcements = []
-    @classrooms.each do |classroom|
-      if !classroom.classroom_announcements.empty?
-        @classroom_announcements << classroom.classroom_announcements
-      end
-    end
-    @filtered_announcements = @classroom_announcements.flatten
-
-    @unread_announcements = @filtered_announcements.reject{ |announcement| announcement.created_at <= @user.last_sign_in_at }
-  end
-
   private
 
   def skip_pundit?
